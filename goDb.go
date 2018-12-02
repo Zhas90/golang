5175b99b0d9dce4740e2cdb06e25e7588f7349f7
Zhas <zh.baimanov@gmail.com>
2018-12-03 00:19:21 +0600

    Update goDb.go

--- goDb.go
+++ goDb.go
@@ -2,18 +2,50 @@ package main
 
 import (
     "database/sql"
-    "fmt"
     _ "gopkg.in/goracle.v2"
+    "github.com/gorilla/mux"
+    "encoding/json"
+    "net/http"
+    "log"
 )
 
+type Customer struct {
+	 NAME      string `json:"name"`
+	 STATE     string `json:"state"`
+	 PAN       string `json:"pan"`
+   GUID      string `json:"guid"`
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+   w.Header().Set("Content-Type", "application/json")
+
+   db, err := sql.Open("goracle", "username/password@localhost:1521/xe")
+   if err != nil {
+     log.Println(err)
+     return
+   }
+   defer db.Close()
+
+   params := mux.Vars(r)
+   customerId := params["customerId"]
+   rows, err := db.Query("SELECT name, state, pan, guid FROM rbs.customers c WHERE c.customerId = ?", customerId)
+   if err != nil {
+      log.Println("Error fetching addition")
+      log.Println(err)
+      return
+   }
+   defer rows.Close()
+
+   for rows.Next() {
+      var customer Customer
+      rows.Scan(&customer.NAME, &customer.STATE, &customer.PAN, &customer.GUID)
+      json.NewEncoder(w).Encode(customer)
+      return
+   }
+}
+
 func main() {
-    dsn := `system/manager@//1.2.3.4:1521/xe`
-
-    _, err := sql.Open("goracle", dsn)
-    if err != nil {
-        fmt.Println(err.Error())
-        return
-    }
-
-    fmt.Println("successfully connected to oracle server using dsn:", dsn)
+   r := mux.NewRouter()
+   r.HandleFunc("/customers/{customerId}", getCustomer).Methods("GET")
+   log.Fatal(http.ListenAndServe(":8000", r))
 }
